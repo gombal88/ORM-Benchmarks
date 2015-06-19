9511bf63bbeeb5af33bd58b4c1f671f2559a02a2
gombal88 <gombalgreat@gmail.com>
2015-06-19 18:19:36 +0200

    Initial Commit

---
+++ app/src/androidTest/java/pl/gombal/orm_benchmarks/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.gombal.orm_benchmarks;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
